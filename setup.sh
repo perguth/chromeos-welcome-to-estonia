d4f20789f3084a94bab7106b50c5dc44e979a009
Per Guth <mail@perguth.de>
2022-08-21 01:11:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt update && sudo apt install -y ansible
-ansible-galaxy install alzadude.firefox-addon
 
 # Clone repository
 git clone https://github.com/perguth/chromeos-welcome-to-estonia.git
