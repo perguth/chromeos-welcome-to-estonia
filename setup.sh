bafe76bb4f8965f3397ebaa98b36bee577ab507c
Per Guth <mail@perguth.de>
2024-06-05 21:36:29 +0200

    Remove custom repo for Ansible

--- setup.sh
+++ setup.sh
@@ -5,12 +5,7 @@ NAME=chromeos-welcome-to-estonia
 # Clean up
 trap "cd .. && rm -rf $NAME" EXIT
 
-# Temporary bugfix as of `02/28/2020`
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
-
 # Install Ansible
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt update && sudo apt install -y ansible
 
 # Clone repository
